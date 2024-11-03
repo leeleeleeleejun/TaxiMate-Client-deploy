5ccd7b0505bd831803ec9122e72319b04963ede0
leeleeleeleejun <wnstjr6293@naver.com>
2024-11-03 17:31:04 +0900

    feat: onboarding 이미지 추가 및 메인으로 이동 함수 추가

--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -1,19 +1,34 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import reactNativePostMessage from '@/utils/reactNavtivePostMessage.ts';
+
+import Header from '@/components/common/Layout/Header';
+import { HeaderItem } from '@/components/common/Layout/Header/Header.style.ts';
 import { JoinButton } from '@/components/PostDetail/PostDetail.style.ts';
-import styled from 'styled-components';
+
+import TaxiIcon from '@/assets/icons/header/taxi-icon.svg?react';
+import KnuLogoIcon from '@/assets/icons/header/knu-logo-icon.svg?react';
 
 const OnboardingPage = () => {
   const sliderRef = useRef<Slider | null>(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const navigate = useNavigate();
 
   const next = () => {
     if (sliderRef.current) {
+      if (currentSlide === 2) {
+        navigate('/');
+      }
       sliderRef.current.slickNext();
+      setCurrentSlide((prev) => prev + 1);
     }
   };
+
   const settings = {
     dots: true,
     arrows: false,
@@ -21,44 +36,64 @@ const OnboardingPage = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    height: 100,
   };
 
   return (
     <>
-      <Slider
-        ref={(slider) => {
-          sliderRef.current = slider;
-        }}
-        {...settings}
-      >
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-      <NextButton style={{ margin: '100px' }} onClick={next}>
-        Next
-      </NextButton>
+      <Header>
+        <HeaderItem>
+          택시팟
+          <TaxiIcon />
+        </HeaderItem>
+        <button
+          onClick={() => {
+            reactNativePostMessage('like_knu');
+          }}
+        >
+          <KnuLogoIcon />
+        </button>
+      </Header>
+      <Main>
+        <Slider
+          ref={(slider) => {
+            sliderRef.current = slider;
+          }}
+          {...settings}
+          useCSS={true}
+        >
+          <div>
+            <Image src={'/onboarding1.png'} />
+          </div>
+          <div>
+            <Image src={'/onboarding2.png'} />
+          </div>
+          <div>
+            <Image src={'/onboarding3.png'} />
+          </div>
+        </Slider>
+        <NextButton onClick={next}>
+          {currentSlide === 2 ? '완료' : '다음'}
+        </NextButton>
+      </Main>
     </>
   );
 };
 export default OnboardingPage;
 
 const NextButton = styled(JoinButton)`
-  margin: auto;
-  height: 40px;
+  margin: 0 auto;
+  width: 200px;
+  min-height: 40px;
+`;
+
+const Image = styled.img`
+  margin-bottom: 60px;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
 `;
