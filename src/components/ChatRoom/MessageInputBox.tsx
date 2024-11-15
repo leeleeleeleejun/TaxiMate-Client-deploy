d70e23fc56b571f0061984f05f7b9cd27d76d34a
leeleeleeleejun <wnstjr6293@naver.com>
2024-11-15 15:24:08 +0900

    fix: ios 한글 조합 문제 해결

--- src/components/ChatRoom/MessageInputBox.tsx
+++ src/components/ChatRoom/MessageInputBox.tsx
@@ -1,8 +1,9 @@
+import { useRef, useState } from 'react';
+
 import {
   MessageInput,
   MessageInputBoxContainer,
 } from '@/components/ChatRoom/chatRoom.style.ts';
-import { useRef, useState } from 'react';
 
 import ArrowUpIcon from '@/assets/icons/chat/arrow-up-icon.svg?react';
 
@@ -15,14 +16,14 @@ const MessageInputBox = ({
 }) => {
   const [input, setInput] = useState('');
   const inputRef = useRef<HTMLTextAreaElement>(null);
+  const hiddenInput = useRef<HTMLInputElement>(null);
   const sendMessageFunc = () => {
     if (input.trim()) {
       sendMessage(partyId, input);
 
       setInput('');
-      if (inputRef.current) {
-        inputRef.current.focus();
-      }
+      hiddenInput.current?.focus();
+      inputRef.current?.focus();
     } else {
       console.log('유효하지 않은 메시지이거나 파티입니다.');
     }
@@ -36,6 +37,10 @@ const MessageInputBox = ({
         onChange={(e) => setInput(e.target.value)}
         $inputLineLength={input.split('\n').length}
         ref={inputRef}
+      />
+      <input
+        ref={hiddenInput}
+        style={{ position: 'absolute', opacity: 0, height: 0 }}
       />
       <button onClick={sendMessageFunc}>
         <ArrowUpIcon />
