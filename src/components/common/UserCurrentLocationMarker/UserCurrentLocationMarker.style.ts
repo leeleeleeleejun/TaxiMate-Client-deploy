28f4e049218f3454cd64a1bf690284881d66c7ce
leeleeleeleejun <wnstjr6293@naver.com>
2024-12-20 20:55:22 +0900

    design: 사용자 현재 위치 마커 컴포넌트 생성

---
+++ src/components/common/UserCurrentLocationMarker/UserCurrentLocationMarker.style.ts
@@ -0,0 +1,20 @@
+const UserCurrentLocationMarkerStyle = `
+  .user-marker-icon-container {
+    display: flex;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;  
+    background-color: rgba(49, 130, 247, 0.4);
+  }
+  .user-marker-icon {
+    margin: auto;
+    width: 18px;
+    height: 18px;
+    border-radius: 50%;  
+    border: 2px solid var(--color-white);
+    background-color: var(--color-blue);
+  }
+`;
+
+export default UserCurrentLocationMarkerStyle;
