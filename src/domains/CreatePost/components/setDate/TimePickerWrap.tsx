547e00a74e264b54250909766e79591494f8b311
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-20 18:22:14 +0900

    refactor: TimePickerSelectionItem 상수명 스네이크 케이스로 수정

--- src/domains/CreatePost/components/setDate/TimePickerWrap.tsx
+++ src/domains/CreatePost/components/setDate/TimePickerWrap.tsx
@@ -1,6 +1,6 @@
 import { SelectionKey } from '@/types';
 import Picker from 'react-mobile-picker';
-import { selections } from '../../constants/TimePickerSelectionItem.ts';
+import { TIME_PICKER_SELECTION_ITEM } from '../../constants/TimePickerSelectionItem.ts';
 
 interface Props {
   time: { meridiem: string; hour: string; minute: string };
@@ -12,15 +12,17 @@ interface Props {
 const TimePickerWrap = ({ time, setTime }: Props) => {
   return (
     <Picker value={time} onChange={setTime} height={90}>
-      {(Object.keys(selections) as SelectionKey[]).map((name) => (
-        <Picker.Column key={name} name={name}>
-          {selections[name].map((option) => (
-            <Picker.Item key={option} value={option}>
-              {option}
-            </Picker.Item>
-          ))}
-        </Picker.Column>
-      ))}
+      {(Object.keys(TIME_PICKER_SELECTION_ITEM) as SelectionKey[]).map(
+        (name) => (
+          <Picker.Column key={name} name={name}>
+            {TIME_PICKER_SELECTION_ITEM[name].map((option) => (
+              <Picker.Item key={option} value={option}>
+                {option}
+              </Picker.Item>
+            ))}
+          </Picker.Column>
+        )
+      )}
     </Picker>
   );
 };
--- src/domains/CreatePost/constants/TimePickerSelectionItem.ts
+++ src/domains/CreatePost/constants/TimePickerSelectionItem.ts
@@ -1,6 +1,6 @@
 import { SelectionKey } from '@/types';
 
-export const selections: Record<SelectionKey, string[]> = {
+export const TIME_PICKER_SELECTION_ITEM: Record<SelectionKey, string[]> = {
   meridiem: ['AM', 'PM'],
   hour: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
   minute: [
