36ec161e9ae25af7c42a2749f0a951e73273ad52
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-31 16:35:27 +0900

    refactor: layout에서 message이벤트 관련 로직 삭제

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,52 +1,7 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Container from '@/components/common/Layout/Layout.style.ts';
-import { useEffect } from 'react';
-import { CLIENT_PATH } from '@/constants/path.ts';
-import reactNativePostMessage from '@/utils/reactNativePostMessage.ts';
-import { useSetPushAlarmMutation } from '@/api/userApi.ts';
 
 const Layout = () => {
-  const navigate = useNavigate();
-
-  const [setPushAlarmTrigger] = useSetPushAlarmMutation();
-
-  useEffect(() => {
-    const handleMessage = (e: MessageEvent) => {
-      if (
-        e.origin === 'https://vercel.live' ||
-        /^react-devtools/.test(e.data.source)
-      ) {
-        return;
-      }
-
-      try {
-        const { data, type } = JSON.parse(e.data);
-
-        switch (type) {
-          case 'CHAT':
-            if (data.partyId) {
-              navigate(
-                CLIENT_PATH.CHAT_ROOM.replace(':chatRoomId', data.partyId)
-              );
-            }
-            break;
-          case 'PUSH_NOTIFICATION':
-            if (data.token) {
-              setPushAlarmTrigger(data.token);
-            }
-            break;
-        }
-      } catch (error) {
-        console.error('메시지 처리 중 오류 발생:', error);
-      }
-    };
-
-    window.addEventListener('message', handleMessage);
-    reactNativePostMessage('chat');
-
-    return () => window.removeEventListener('message', handleMessage);
-  }, []);
-
   return (
     <Container>
       <Outlet />
