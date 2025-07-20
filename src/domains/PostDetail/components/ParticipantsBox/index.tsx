8e627835fb61c7064d9816323e639fae3cbf7d6b
leeleeleeleejun <wnstjr6293@naver.com>
2025-07-20 11:42:52 +0900

    fix: ParticipantsBox 배열 복사본 생성 시 문법 오류 수정

--- src/domains/PostDetail/components/ParticipantsBox/index.tsx
+++ src/domains/PostDetail/components/ParticipantsBox/index.tsx
@@ -5,7 +5,7 @@ import { Participant } from '@/types/post.ts';
 const ParticipantsBox = ({ participants }: { participants: Participant[] }) => {
   return (
     <ParticipantsBoxContainer>
-      {participants // 배열의 복사본을 생성
+      {[...participants] // 배열의 복사본을 생성
         .sort((a) => (a.role === 'HOST' ? -1 : 1)) // 'HOST'인 데이터를 앞으로 이동
         .map((item) => (
           <UserContainer
